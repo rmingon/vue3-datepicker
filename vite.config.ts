1df85c9f8ccaf306a41a9d68451642adf9fb7268
Ronan <ronanmingon69550@gmail.com>
2023-02-24 11:40:09 +0100

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'VueDatepicker',
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => `vue3-datepicker.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
